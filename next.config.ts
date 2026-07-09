a6f3367165db5496f39255aa3bc56acefbc9cc88
Rum Chen <i.mr.redrum@gmail.com>
2026-07-09 17:46:47 +0800

    feat: add GitHub Actions workflow for deploying Next.js site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+const isProd = process.env.NODE_ENV === 'production'
+const PROJECT_NAME = 'Demo_Pet-Map'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
 
-export default nextConfig;
+  // Setting up GitHub Pages
+  output: 'export',
+  basePath: isProd ? `/${PROJECT_NAME}` : '',
+  assetPrefix: isProd ? `/${PROJECT_NAME}/` : '',
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
